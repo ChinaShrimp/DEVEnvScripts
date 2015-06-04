268da12a60915bbc51b3a0e67778f8e14d20a903
Lyon Liang <lyon.liang12@gmail.com>
2015-06-04 22:41:54 +0800

    add git configuration

---
+++ git.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+
+yum install -y git
+
+git config --global user.name "Lyon Liang"
+git config --global user.email "lyon.liang12@gmail.com"
+git config --global alias.ci commit
+git config --global alias.co checkout
+git config --global alias.st status
+git config --global alias.br branch
+
