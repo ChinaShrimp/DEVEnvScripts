90d360455b9842e6419cf570f06e1a58b1ed12fb
Lyon Liang <lyon.liang12@gmail.com>
2015-06-04 11:32:40 -0400

    bug fix for epel

--- python.sh
+++ python.sh
@@ -2,16 +2,17 @@
 
 # for CentOS and BASH
 
-yum install -y gcc python python-devel python-pip
+yum install -y epel-release gcc python python-devel python-pip
 pip install virtualenv virtualenvwrapper
 
 mkdir -p $HOME/.virtualenvs
 
 echo "export WORKON_HOME=~/.virtualenvs" >> $HOME/.bash_profile
-echo "source /usr/local/bin/virtualenvwrapper.sh" >> $HOME/.bash_profile
+echo "source /usr/bin/virtualenvwrapper.sh" >> $HOME/.bash_profile
 
 source $HOME/.bash_profile
 
+echo "=================="
 echo "now you can use:"
 echo "mkvirtualenv <env>"
 echo "workon <env>"
