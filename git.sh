0a5b9b73359cf92b29d33f2ff47a8db67f5e02cf
Lyon Liang <lyon.liang12@gmail.com>
2015-06-04 22:42:41 +0800

    add push setting

--- git.sh
+++ git.sh
@@ -8,4 +8,4 @@ git config --global alias.ci commit
 git config --global alias.co checkout
 git config --global alias.st status
 git config --global alias.br branch
-
+git config --global push.default matching
